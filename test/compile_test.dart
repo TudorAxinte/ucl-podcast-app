37efbf7263c792a7a179395f2a5c1318a02ecdb3
Tudor Axinte <tudor.2001@yahoo.com>
2022-02-05 15:59:33 +0000

    CI/CD

---
+++ test/compile_test.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../lib/main.dart';
+import 'mock/firebase_mock.dart';
+
+void main() {
+  testWidgets('Expect no errors thrown', (WidgetTester tester) async {
+
+    setupFirebaseAuthMocks();
+    await Firebase.initializeApp();
+    await tester.pumpWidget(PodcastApp());
+
+    expect(find.text('Error'), findsNothing);
+  });
+}
